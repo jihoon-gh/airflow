ebbc4875fb08d0b45499137a4fb7d365014be4fa
jihoon-gh <jihoon806@gmail.com>
2025-09-17 16:00:07 +0900

    add dag with EmptyOperators

---
+++ test_dag.py
@@ -0,0 +1,47 @@
+from airflow import DAG
+from airflow.providers.standard.operators.empty import EmptyOperator
+import datetime
+import pendulum
+
+with DAG(
+    dag_id="test_dag",
+    catchup=False,
+    schedule=None,
+    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul")
+) as dag:
+    
+    t1 = EmptyOperator(
+        task_id="t1"
+    )
+
+    t2 = EmptyOperator(
+        task_id="t2"
+    )
+
+    t3 = EmptyOperator(
+        task_id="t3"
+    )
+
+    t4 = EmptyOperator(
+        task_id="t4"
+    )
+
+    t5 = EmptyOperator(
+        task_id="t5"
+    )
+
+    t6 = EmptyOperator(
+        task_id="t6"
+    )
+
+    t7 = EmptyOperator(
+        task_id="t7"
+    )
+
+    t8 = EmptyOperator(
+        task_id="t8"
+    )
+
+    t1 >> [t2, t3] >> t4
+    t5 >> t4
+    [t4, t7] >> t8
